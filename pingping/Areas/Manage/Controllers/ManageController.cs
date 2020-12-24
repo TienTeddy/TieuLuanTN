366ae6ad28d1b16cb6ea89fb42ed495c5366f175
TienTeddy <Tienle10998@gmail.com>
2020-12-24 19:50:28 +0700

    cart

---
+++ pingping/Areas/Manage/Controllers/ManageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pingping.Areas.Manage.Controllers
+{
+    public class ManageController : Controller
+    {
+        // GET: Manage/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Icons()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Tables()
+        {
+            return View();
+        }
+    }
+}
